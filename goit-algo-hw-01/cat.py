8e5987f9e267bec94e07749177008aaf4eb50559
khomasl <gymnasium.yv@gmail.com>
2026-01-30 14:42:40 +0200

    hw-01

---
+++ goit-algo-hw-01/cat.py
@@ -0,0 +1,12 @@
+cat = {}
+info = {"status": "vaccinated", "breed": True}
+
+cat["nick"] = "Simon"
+cat["age"] = 7
+cat["characteristics"] = ["лагідний", "кусається"]
+
+age = cat.get("age")
+
+cat.update(info)
+
+print(cat)
---
+++ goit-algo-hw-01/main.py
@@ -0,0 +1,12 @@
+length = "2.75"
+width = "1.75"
+area = float(length) * float(width)
+show = f"With width {width} and length {length} of the room, its area is equal to {area}"
+print(show)
+
+name = input("Your name? ")
+email = input("Your email? ")
+age = int(input("Your age? "))
+height = float(input("Your height? "))
+is_active = bool(input("Is active? "))
+print(is_active)
